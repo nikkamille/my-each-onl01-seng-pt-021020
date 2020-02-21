a4bf1c6f346fbd6aa66df928e076fe7d71cab2bd
Kamille Longalong <k.pagibigan@gmail.com>
2020-02-21 07:34:50 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,8 @@
 def my_each(array) # put argument(s) here
-  if block_given?
-    k = 0
-    while k < array.length
-      yield(array[k])
-      k += 1
-    end
-  else 
-    
+  k = 0
+  while k < array.length
+    yield(array[k])
+    k += 1
   end
+  array
 end
